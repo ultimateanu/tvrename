461210ad8b1f29650b21c1f4e1fa37f46bd53387
Anu Bandi <ultimateanu@gmail.com>
2014-07-20 21:58:10 -0700

    parsing os args

--- tvrename.go
+++ tvrename.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"fmt"
-	//"io/ioutil"
-	//"log"
-	//"net/http"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	//fmt.Println("hello world\n")
-	//findShow("pie")
-	//getResource("http://www.google.com/robots.txt")
+	//fmt.Println(findShow("legend of korra$%"))
+	if len(os.Args) < 2 {
+		log.Fatal("usage: tvrename show_directory")
+	}
+	fixShowDir(os.Args[1])
+}
 
-	fmt.Println(findShow("legend of korra$%"))
+func fixShowDir(dir string) {
+	//fmt.Println(filepath.Base(dir))
+	fmt.Println(findShow(filepath.Base(dir)))
 }
